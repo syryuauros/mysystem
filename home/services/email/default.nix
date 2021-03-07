620c7c706cb0dceca8dbcb9b7a94432eda5fda4b
JJ <wavetojj@gmail.com>
2021-03-08 01:26:07 +0900

    external browser for notmuch configured

--- home/services/email/default.nix
+++ home/services/email/default.nix
@@ -38,76 +38,10 @@ in {
     (mkDaum { name = "haedosa"; })
   ];
 
-  # imports = [
-  #   (./gmails "wavetojj")
-  # ];
 
-#   imports =
-
-# [
-#   {
-#     accounts.email.accounts.wavetojj = {
-#       flavor = "gmail.com";
-#       address = "wavetojj@gmail.com";
-#       realName = "JJ Kim";
-#       gpg = {
-#         key = "6A9DC1FC403B1F49";
-#         signByDefault = false;
-#       };
-#       mbsync = {
-#         enable = true;
-#         create = "maildir";
-#       };
-#       msmtp.enable = true;
-#       notmuch.enable = true;
-#       primary = true;
-#       passwordCommand = "pass email/wavetojj@gmail.com";
-#       # imap.host = "imap.gmail.com";
-#       # smtp.host = "smtp.gmail.com";
-#       signature = {
-#         text = ''
-#           JJ Kim
-#         '';
-#         showSignature = "append";
-#       };
-#     };
-#   }
-# ];
-
-
-
-
-
-
-  # accounts.email = {
-
-  #   maildirBasePath = "Mail";
-
-  #   accounts.wavetojj = {
-  #     flavor = "gmail.com";
-  #     address = "wavetojj@gmail.com";
-  #     realName = "JJ Kim";
-  #     gpg = {
-  #       key = "6A9DC1FC403B1F49";
-  #       signByDefault = false;
-  #     };
-  #     mbsync = {
-  #       enable = true;
-  #       create = "maildir";
-  #     };
-  #     msmtp.enable = true;
-  #     notmuch.enable = true;
-  #     primary = true;
-  #     passwordCommand = "pass email/wavetojj@gmail.com";
-  #     # imap.host = "imap.gmail.com";
-  #     # smtp.host = "smtp.gmail.com";
-  #     signature = {
-  #       text = ''
-  #         JJ Kim
-  #       '';
-  #       showSignature = "append";
-  #     };
-  #   };
-  # };
+  home.file.".mailcap".text =
+    if pkgs.stdenv.hostPlatform.isLinux
+      then "text/html; qutebrowser %s; nametemplate=%s.html"
+      else "text/html; open %s; nametemplate=%s.html";
 
 }
