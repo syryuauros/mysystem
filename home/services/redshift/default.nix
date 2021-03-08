6cb76b361cd53e300a20c24ed28c42a442cd5896
JJ <wavetojj@gmail.com>
2021-03-08 20:46:54 +0900

    redshift adjusted

--- home/services/redshift/default.nix
+++ home/services/redshift/default.nix
@@ -8,10 +8,10 @@
     dawnTime = "6:00-7:45";
     duskTime = "18:35-20:15";
     temperature.day = 5500;
-    temperature.night = 3700;
+    temperature.night = 4500;
 
-    settings.redshift.brightness-day = "0.8";
-    settings.redshift.brightness-night = "0.6";
+    settings.redshift.brightness-day = "1.0";
+    settings.redshift.brightness-night = "1.0";
     # latitude = "0.0000";
     # longitude = "0.0000";
     provider = "geoclue2";
