d00cb1ac045d9e84c60a605c28477833fc166436
JJ Kim <wavetojj@gmail.com>
2021-05-31 01:09:48 +0900

    picom changed to yshui's version

--- home/services/picom/default.nix
+++ home/services/picom/default.nix
@@ -19,43 +19,43 @@ let
 in {
 
   # for jonaburg version
-  services.picom = {
-    enable = true;
-    package = picom-jonaburg;
-    backend = "glx";
-    experimentalBackends = true;
-
-    fade = true;
-    fadeDelta = 5;
-
-    activeOpacity = "0.95";
-    inactiveOpacity = "0.8";
-    opacityRule = [
-      "95:class_g = 'emacs'"
-      "95:class_i = 'Firefox'"
-      "95:class_g = 'firefox'"
-      "95:class_g = 'Navigator'"
-      "95:class_g = 'Alacritty'"
-    ];
-
-    shadow = true;
-    shadowOpacity = "0.75";
-
-    extraOptions = builtins.readFile ./picom-jonaburg-extra.conf;
-
-  };
-
-  # for yshui version, shipped in nixpkgs by default
   # services.picom = {
   #   enable = true;
-  #   activeOpacity = "0.95";
-  #   inactiveOpacity = "0.8";
+  #   package = picom-jonaburg;
   #   backend = "glx";
+  #   experimentalBackends = true;
+
   #   fade = true;
   #   fadeDelta = 5;
-  #   # opacityRule = [ "100:name *= 'i3lock'" ];
+
+  #   activeOpacity = "0.95";
+  #   inactiveOpacity = "0.8";
+  #   opacityRule = [
+  #     "95:class_g = 'emacs'"
+  #     "95:class_i = 'Firefox'"
+  #     "95:class_g = 'firefox'"
+  #     "95:class_g = 'Navigator'"
+  #     "95:class_g = 'Alacritty'"
+  #   ];
+
   #   shadow = true;
   #   shadowOpacity = "0.75";
+
+  #   extraOptions = builtins.readFile ./picom-jonaburg-extra.conf;
+
   # };
+
+  #for yshui version, shipped in nixpkgs by default
+  services.picom = {
+    enable = true;
+    activeOpacity = "0.95";
+    inactiveOpacity = "0.8";
+    backend = "glx";
+    fade = true;
+    fadeDelta = 5;
+    # opacityRule = [ "100:name *= 'i3lock'" ];
+    shadow = true;
+    shadowOpacity = "0.75";
+  };
 
 }
