a7c4f32512457c0679619c7b6789d26ef3646c92
JJ Kim <wavetojj@gmail.com>
2021-07-16 09:40:30 +0900

    remove fish as a default shell for jj

--- machines/linux/users/default.nix
+++ machines/linux/users/default.nix
@@ -8,7 +8,6 @@
     home = "/home/jj";
     extraGroups = [ "wheel" "networkmanager" ];
     hashedPassword = "$6$3nKguLgJMB$leFSKrvWiUAXiay8MJ8i66.ZzufIhkrrbxzv625DV28xSYGBCLp62pyIp4U3s8miHcOdJZpWLgDMEoWljPtT0.";
-    shell = pkgs.fish;
   };
 
   users.extraUsers.jj.extraGroups = [ "audio" "video" ];
