a570a0e2b76ac823defff69a7fc15e7f8703b7fc
JJ Kim <wavetojj@gmail.com>
2021-07-17 22:57:53 +0900

    trackpad and the middle button of trackpoint disabled for l14

--- home/linux/l14/default.nix
+++ home/linux/l14/default.nix
@@ -23,11 +23,14 @@
   xsession.initExtra = ''
 
     # make trackpad and trackpoint faster
-    xinput set-prop "ELAN0672:00 04F3:3187 Touchpad" 335 1
+    xinput set-prop "SynPS/2 Synaptics Touchpad" 335 1
     xinput set-prop "TPPS/2 Elan TrackPoint" 335 0.5
 
-    # disble the trackpad
-    xinput set-prop "ELAN0672:00 04F3:3187 Touchpad" "Device Enabled" 0
+    # disable the trackpad
+    xinput set-prop "SynPS/2 Synaptics Touchpad" "Device Enabled" 0
+
+    # disable the middle button click of the trackpoint
+    xinput set-button-map "TPPS/2 Elan TrackPoint" 1 0 3
 
     setxkbmap -option altwin:swap_lalt_lwin -option caps:ctrl_modifier
 
