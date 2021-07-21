cad4227f5b671ec69689464f203556640d90f966
JJ Kim <wavetojj@gmail.com>
2021-07-21 19:31:11 +0900

    all-the-icons-dired; lsp-ui-doc-mode for haskell configured

--- home/programs/doom-emacs/doom.d/packages.el
+++ home/programs/doom-emacs/doom.d/packages.el
@@ -48,3 +48,8 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+(package! envrc)
+(package! lsp-haskell)
+(package! lsp-ui)
+(package! all-the-icons-dired)
