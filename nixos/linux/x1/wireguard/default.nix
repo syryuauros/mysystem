ba515c0913466352bcb278fd6de95b8804bf8840
JJ Kim <wavetojj@gmail.com>
2021-08-07 21:48:16 +0900

    wg-quick tried but still does not work

--- nixos/linux/x1/wireguard/default.nix
+++ nixos/linux/x1/wireguard/default.nix
@@ -1,45 +1,63 @@
 { ... }:
 {
 
-  networking.firewall = {
-    allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
-    allowedTCPPorts = [ 80 8081 ];
-  };
-  # Enable WireGuard
-  networking.wireguard.interfaces = {
-    # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg0 = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.100.0.2/24" ];
-      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+  # networking.firewall = {
+  #   allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
+  # };
 
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
-      privateKeyFile = "/home/jj/wireguard-keys/private";
+  # # Enable WireGuard
+  # networking.wireguard.interfaces = {
+  #   # "wg0" is the network interface name. You can name the interface arbitrarily.
+  #   wg0 = {
+  #     # Determines the IP address and subnet of the client's end of the tunnel interface.
+  #     ips = [ "10.100.0.2/24" ];
+  #     listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
 
-      peers = [
-        # For a client configuration, one peer entry for the server will suffice.
+  #     # Path to the private key file.
+  #     #
+  #     # Note: The private key can also be included inline via the privateKey option,
+  #     # but this makes the private key world-readable; thus, using privateKeyFile is
+  #     # recommended.
+  #     privateKeyFile = "/home/jj/wireguard-keys/private";
 
-        {
-          # Public key of the server (not a file path).
-          publicKey = "tzSHRgkDJwo1u2W3L9eVyOOxeU/FrD4ppHSyirLkzDw=";
+  #     peers = [
+  #       # For a client configuration, one peer entry for the server will suffice.
 
-          # Forward all the traffic via VPN.
-          allowedIPs = [ "0.0.0.0/0" ];
-          # Or forward only particular subnets
-          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
+  #       {
+  #         # Public key of the server (not a file path).
+  #         publicKey = "iFb4Ua34391/8hwCY3F3a3ceMZggkvZqo4dQZqi3ATo=";
 
-          # Set this to the server IP and port.
-          endpoint = "13.124.118.167:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+  #         # Forward all the traffic via VPN.
+  #         allowedIPs = [ "0.0.0.0/0" ];
+  #         # Or forward only particular subnets
+  #         #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
 
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
+  #         # Set this to the server IP and port.
+  #         endpoint = "13.124.118.167:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+
+  #         # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+  #         persistentKeepalive = 25;
+  #       }
+  #     ];
+  #   };
+  # };
+
+  # networking.wg-quick.interfaces = {
+  #   wg0 = {
+  #     address = [ "10.0.0.2/24" "fdc9:281f:04d7:9ee9::2/64" ];
+  #     dns = [ "10.0.0.1" "fdc9:281f:04d7:9ee9::1" ];
+  #     privateKeyFile = "/home/jj/wireguard-keys/private";
+
+  #     peers = [
+  #       {
+  #         publicKey = "iFb4Ua34391/8hwCY3F3a3ceMZggkvZqo4dQZqi3ATo=";
+  #         # presharedKeyFile = "/root/wireguard-keys/preshared_from_peer0_key";
+  #         allowedIPs = [ "0.0.0.0/0" "::/0" ];
+  #         endpoint = "13.124.118.167:51820";
+  #         persistentKeepalive = 25;
+  #       }
+  #     ];
+  #   };
+  # };
 
 }
