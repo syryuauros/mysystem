f44ef6c777ed1149eb61dce1a66e572d48c98899
JJ Kim <wavetojj@gmail.com>
2021-08-19 23:51:39 +0900

    fancy fonts are removed

--- packages/myfonts/overlay.nix
+++ packages/myfonts/overlay.nix
@@ -1,7 +1,27 @@
 final: prev:
-with final; {
+with final; let
+
+  mkFont = path: runCommand "myfont" {} ''
+    target=$out/share/fonts/myfont
+    mkdir -p $target
+    cp ${path}/*.ttf ${path}/*.otf $target
+  '';
+
+in {
 
   myfonts = callPackage ./deriv.nix {};
+
+  cute-font      = mkFont ./fonts/Cute_Font;
+  hi-melody      = mkFont ./fonts/Hi_Melody;
+  kirang-haerang = mkFont ./fonts/Kirang_Haerang;
+  nanum-gothic   = mkFont ./fonts/Nanum_Gothic;
+  nanum-myeongjo = mkFont ./fonts/Nanum_Myeongjo;
+  noto-sans-kr   = mkFont ./fonts/Noto_Sans_KR;
+  noto-serif-kr  = mkFont ./fonts/Noto_Serif_KR;
+  seoul-hangan   = mkFont ./fonts/Seoul_Hangan;
+  single-day     = mkFont ./fonts/Single_Day;
+  sunflower      = mkFont ./fonts/Sunflower;
+  yeon-sung      = mkfont ./fonts/Yeon_Sung;
 
   mynerdfonts = prev.nerdfonts.override {
     fonts = [
@@ -13,10 +33,13 @@ with final; {
   };
 
   myfonts-collection = with final; [
-    myfonts
+    # myfonts
+    noto-sans-kr
+    noto-serif-kr
     # nerdfonts
     mynerdfonts
-    noto-fonts
+    # noto-fonts-cjk
+    # noto-fonts
     material-design-icons
     weather-icons
     font-awesome
