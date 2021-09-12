2f46542591a82a6673ea10773051490865b72ebb
JJ Kim <wavetojj@gmail.com>
2021-09-12 15:18:04 +0900

    builder1 and builder2 configured

--- nixos/linux/distributed-build/default.nix
+++ nixos/linux/distributed-build/default.nix
@@ -5,7 +5,17 @@
   nix.distributedBuilds = true;
   nix.buildMachines = [
     {
-      hostName = "10.100.0.5";
+      hostName = "10.100.100.1";
+      sshUser = "root";
+      sshKey = "/home/jj/.ssh/id_builder";
+      system = "x86_64-linux";
+      maxJobs = 6;
+      speedFactor = 2;
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [ ];
+    }
+    {
+      hostName = "10.100.100.2";
       sshUser = "root";
       sshKey = "/home/jj/.ssh/id_builder";
       system = "x86_64-linux";
@@ -19,6 +29,7 @@
     builders-use-substitutes = true
   '';
 
-  services.openssh.knownHosts."10.100.0.5".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOy9IObSEcyb3+3gGXuG8uUGIUiWAuW6hPjoq0059SvZ";
+  services.openssh.knownHosts."10.100.100.1".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOy9IObSEcyb3+3gGXuG8uUGIUiWAuW6hPjoq0059SvZ";
+  services.openssh.knownHosts."10.100.100.2".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpvlvAsXnWatOi0JLmrzy6ri443CuvujgW4k86i91Sn";
 
 }
