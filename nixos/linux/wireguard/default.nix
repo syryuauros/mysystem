08d2bda07d36eacf708444259391d165644b7dc4
JJ Kim <jj@haedosa.xyz>
2021-10-03 12:47:51 +0900

    Additional wireguard network hds0 added

--- nixos/linux/wireguard/default.nix
+++ nixos/linux/wireguard/default.nix
@@ -1,48 +1,35 @@
-ips : { ... }:
+haedosa0ips: hds0ips: { ... }:
 {
 
   networking.firewall = {
-    allowedUDPPorts = [ 51820 ];
+    allowedUDPPorts = [ 51820 51821 ];
   };
 
   networking.wireguard.interfaces = {
 
     haedosa0 = {
-      ips = ips;
+      ips = haedosa0ips;
       listenPort = 51820;
-
-      # Create your wireguard keys
-      # ```sh
-      # umask 077
-      # mkdir ~/.wireguard-keys
-      # wg genkey > ~/.wireguard-keys/private
-      # wg pubkey < ~/.wireguard-keys/private > ~/.wireguard-keys/public
-      # ```
       privateKeyFile = "/home/jj/.wireguard-keys/private";
       peers = [
         {
           publicKey = "iFb4Ua34391/8hwCY3F3a3ceMZggkvZqo4dQZqi3ATo=";
-          allowedIPs = [ "10.100.0.1/32"
-                         "10.100.0.2/32"
-                         "10.100.0.22/32"
-                         "10.100.0.3/32"
-                         "10.100.0.4/32"
-                         "10.100.0.6/32"
-                         "10.100.0.7/32"
-                         "10.100.0.8/32"
-                         "10.100.0.9/32"
-                         "10.100.0.10/32"
-                         "10.100.0.11/32"
-                         "10.100.0.100/32"
-
-                         # builders
-                         "10.100.0.5/32" "10.100.100.1/32"  # builder1 (legion 5i)
-                         "10.100.100.2/32"  # builder2 (legion 5i)
-
-                         "10.100.0.0/16"   # This allows all ips starting with 10.100
-
-                       ];
+          allowedIPs = [ "10.100.0.0/16" ];
           endpoint = "3.34.133.42:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+
+    hds0 = {
+      ips = hds0ips;
+      listenPort = 51821;
+      privateKeyFile = "/home/jj/.wireguard-keys/private";
+      peers = [
+        {
+          publicKey = "i0ZorMa8S9fT8/TI/U01K5HGhYPGRESnrq36k2I7MBU=";
+          allowedIPs = [ "10.10.0.0/16" ];
+          endpoint = "121.136.244.64:51821";
           persistentKeepalive = 25;
         }
       ];
