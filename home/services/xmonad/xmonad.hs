34be35fd2016ff236fbb49157554b11dcca39ec1
JJ Kim <jj@haedosa.xyz>
2021-10-12 00:53:54 +0900

    windows focus Up Down key revised for xmonad

--- home/services/xmonad/xmonad.hs
+++ home/services/xmonad/xmonad.hs
@@ -732,12 +732,10 @@ myKeys home conf =
     , ("M-<Return>"   , promote)                   -- Moves focused window to master, others maintain order
     , ("M-S-m"        , swapMaster)                -- Moves focused window to master, others maintain order
     -- , ("M-m"          , windows W.focusMaster)  -- Move focus to the master window
-    , ("M-C-M1-j"       , windows W.focusDown)       -- Move focus to the next window
-    , ("M-C-M1-k"       , windows W.focusUp)         -- Move focus to the prev window
+    , ("M-j"          , windows W.focusDown)       -- Move focus to the next window
+    , ("M-k"          , windows W.focusUp)         -- Move focus to the prev window
     , ("M-m"          , B.focusMaster)             -- Move focus to the master window, skipiping hidden windows
     , ("M-h"          , B.focusUp)                 -- Move focus to the prev window, skipiping hidden windows
-    , ("M-j"          , B.focusDown)               -- Move focus to the next window, skipiping hidden windows
-    , ("M-k"          , B.focusUp)                 -- Move focus to the prev window, skipiping hidden windows
     , ("M-l"          , B.focusDown)               -- Move focus to the next window, skipiping hidden windows
     , ("M-S-h"        , windows W.swapUp)          -- Swap focused window with prev window
     , ("M-S-j"        , windows W.swapDown)        -- Swap focused window with next window
