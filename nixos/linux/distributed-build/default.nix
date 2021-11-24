ca4ddb3591947a2332e8ee28838de0ad97744dde
JJ Kim <jj@haedosa.xyz>
2021-11-24 21:53:29 +0900

    comments removed in distributed-build/default.nix

--- nixos/linux/distributed-build/default.nix
+++ nixos/linux/distributed-build/default.nix
@@ -72,25 +72,10 @@
       IdentityFile /home/jj/.ssh/id_builder
   '';
 
-  # programs.ssh.extraConfig = ''
-  #   Host builder1
-  #     HostName 121.136.244.64
-  #     User root
-  #     Port 101
-  #     IdentityFile /home/jj/.ssh/id_builder
-  #   Host builder2
-  #     HostName 121.136.244.64
-  #     User root
-  #     Port 102
-  #     IdentityFile /home/jj/.ssh/id_builder
-  # '';
-
   services.openssh.knownHosts."10.10.100.1".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOy9IObSEcyb3+3gGXuG8uUGIUiWAuW6hPjoq0059SvZ";
   services.openssh.knownHosts."10.10.100.2".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpvlvAsXnWatOi0JLmrzy6ri443CuvujgW4k86i91Sn";
   services.openssh.knownHosts."10.10.100.3".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAJRy4shsolok43LnXTrwWnPyJ+Gna6JYLQKxO66w8ZN";
   services.openssh.knownHosts."10.10.100.4".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPsGAj/2BqvU6EUKaeW0ojVNl9yIH+N89z5+LvHsnx8k";
   services.openssh.knownHosts."10.10.100.5".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFkD/jGF/atiNoGlN8WVBUinEfntOr6Vs96B2DWnvUO";
-  # services.openssh.knownHosts."[121.136.244.64]:101".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOy9IObSEcyb3+3gGXuG8uUGIUiWAuW6hPjoq0059SvZ";
-  # services.openssh.knownHosts."[121.136.244.64]:102".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGpvlvAsXnWatOi0JLmrzy6ri443CuvujgW4k86i91Sn";
 
 }
