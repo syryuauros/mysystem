d9913f0f78e7783020c8c4279f7d7f004ee5db66
JJ Kim <jj@haedosa.xyz>
2022-04-22 17:04:12 +0900

    inputMethod changed to kime

--- nixos/i18n/default.nix
+++ nixos/i18n/default.nix
@@ -5,8 +5,16 @@
   # Select internationalisation properties.
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    inputMethod.enabled = "uim";
-    inputMethod.uim.toolbar = "gtk-systray";
+    inputMethod.enabled = "kime";
+    inputMethod.kime.config = {
+      engine = {
+        hangul = {
+          layout = "sebeolsik-3-91";
+        };
+      };
+    };
+    # inputMethod.enabled = "uim";
+    # inputMethod.uim.toolbar = "gtk-systray";
     # inputMethod.enabled = "nabi";
     # inputMethod.enabled = "fcitx5";
     # inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ hangul ];
