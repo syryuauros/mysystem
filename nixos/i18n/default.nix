6bf5b8d3a5f72a1ee07784ca3fdcdc0c29da8f82
JJ Kim <jj@haedosa.xyz>
2022-04-25 23:16:11 +0900

    comments removed in nixos/i18n

--- nixos/i18n/default.nix
+++ nixos/i18n/default.nix
@@ -2,7 +2,6 @@
 
 {
 
-  # Select internationalisation properties.
   i18n = {
     defaultLocale = "en_US.UTF-8";
     inputMethod.enabled = "kime";
@@ -21,13 +20,6 @@
         };
       };
     };
-    # inputMethod.enabled = "uim";
-    # inputMethod.uim.toolbar = "gtk-systray";
-    # inputMethod.enabled = "nabi";
-    # inputMethod.enabled = "fcitx5";
-    # inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ hangul ];
-    # inputMethod.enabled = "ibus";
-    # inputMethod.ibus.engines = with pkgs.ibus-engines; [ hangul ];
   };
 
 }
