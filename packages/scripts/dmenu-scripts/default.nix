16fc3a033a5abbdb2078ede0f35198d54d05abd8
JJ Kim <jj@haedosa.xyz>
2022-04-30 01:45:30 +0900

    removed unnecassary comments

--- packages/scripts/dmenu-scripts/default.nix
+++ packages/scripts/dmenu-scripts/default.nix
@@ -6,16 +6,3 @@
     patchShebangs $f
   done
 ''
-
-# { runCommand }: let
-
-#   name = "mysetxkbmap";
-
-# in runCommand name { buidInputs = [ ]; } ''
-#   outdir=$out/bin
-#   mkdir -p $outdir
-#   cp ${./.}/* $outdir
-#   for i in $outdir/*.sh;
-#   do
-#     patchShebangs $i
-#   done
