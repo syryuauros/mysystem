8a14b019c75ede7fab6e29891a12ac2d70ba9469
JJ Kim <jj@haedosa.xyz>
2022-07-06 11:52:42 +0900

    nfs mount to /hds/store removed

--- nixos/nfs/default.nix
+++ nixos/nfs/default.nix
@@ -2,10 +2,10 @@
 
 {
 
-  fileSystems."/hds/store" = {
-    device = "10.10.100.4:/hds/store";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
+  # fileSystems."/hds/store" = {
+  #   device = "10.10.100.4:/hds/store";
+  #   fsType = "nfs";
+  #   options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
+  # };
 
 }
