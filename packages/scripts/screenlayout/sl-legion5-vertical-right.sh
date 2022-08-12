49ff034c9c856925787766f3634cba63f0b9e3ac
JJ Kim <jj@haedosa.xyz>
2022-08-12 10:58:13 +0900

    sl-legion5-vertical-right.sh revised

--- packages/scripts/screenlayout/sl-legion5-vertical-right.sh
+++ packages/scripts/screenlayout/sl-legion5-vertical-right.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate left --output DP-3 --off --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-4 --off --output eDP-1-1 --off --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate left --output DP-3 --off --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-4 --mode 1920x1080 --pos 640x1440 --rotate normal
