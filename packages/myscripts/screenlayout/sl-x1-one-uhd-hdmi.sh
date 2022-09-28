4c9da13d1ef022d5b85facf18641cec993d965f7
JJ Kim <jj@haeodsa.xyz>
2022-09-28 21:53:07 +0900

    add sl-x1-one-uhd-hdmi.sh

---
+++ packages/myscripts/screenlayout/sl-x1-one-uhd-hdmi.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1200 --pos 827x2160 --rotate normal --output HDMI-1 --primary --mode 4096x2160 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --off --output DP-3 --off --output DP-4 --off
