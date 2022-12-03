88e4338ff1ad91c119f8f98f4ab5075ad2eb9c6c
JJ Kim <jj@haeodsa.xyz>
2022-12-03 23:12:05 +0900

    follow nixos-unstable

--- flake.nix
+++ flake.nix
@@ -5,13 +5,13 @@
   inputs = {
 
     haedosa.url = "github:haedosa/flakes";
-    nixpkgs.follows = "haedosa/nixpkgs";
-    home-manager.follows = "haedosa/home-manager";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    # nixpkgs.follows = "haedosa/nixpkgs";
+    # home-manager.follows = "haedosa/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nur.url = "github:nix-community/NUR";
     jupyter_contrib_core = {
