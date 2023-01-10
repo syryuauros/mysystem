ac870818d457fb6aca23e432052843b54c46b55f
JJ Kim <jj@haedosa.xyz>
2023-01-10 12:14:22 +0900

    add kmonad hhkb bluetooth

--- homeManagerConfigurations/jj/features/kmonad/default.nix
+++ homeManagerConfigurations/jj/features/kmonad/default.nix
@@ -3,20 +3,39 @@ let
 
   kmonad = inputs.kmonad.packages.${pkgs.system}.default;
 
-  hhkb-usb = pkgs.substitute {
-    src = ./hhkb.kbd;
-    replacements = [ "--replace" "@hhkb@" "usb-PFU_Limited_HHKB-Hybrid-event-kbd" ];
-  };
+
+  kmonad-hhkb-usb = pkgs.writeShellScriptBin "kmonad-hhkb-usb"
+    (let
+      hhkb-usb = pkgs.substitute {
+        src = ./hhkb.kbd;
+        replacements = [ "--replace" "@device@" "/dev/input/by-id/usb-PFU_Limited_HHKB-Hybrid-event-kbd"
+                         "--replace" "@name@" "HHKB-USB"
+                       ];
+      };
+    in "${kmonad}/bin/kmonad ${hhkb-usb}");
+
+  kmonad-hhkb-blt = pkgs.writeShellScriptBin "kmonad-hhkb-blt"
+    (let
+      hhkb-white = pkgs.substitute {
+        src = ./hhkb.kbd;
+        replacements = [ "--replace" "@device@" "/dev/input/event22"
+                         "--replace" "@name@" "HHKB-Bluetoot"
+                       ];
+      };
+    in "${kmonad}/bin/kmonad ${hhkb-white}");
 
 in {
 
   home.packages = [
     kmonad
+    kmonad-hhkb-usb
+    kmonad-hhkb-blt
   ];
 
   xsession.enable = true;
   xsession.initExtra = ''
-    ${kmonad}/bin/kmonad ${hhkb-usb} &
+    kmonad-hhkb-usb &
+    kmonad-hhkb-blt &
     xset r rate 350 70
   '';
 
