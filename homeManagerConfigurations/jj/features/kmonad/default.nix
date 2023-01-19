f136e0e7a5f5d854ecbcae6c69cd05a5a16dd142
JJ Kim <jj@haedosa.xyz>
2023-01-20 01:15:32 +0900

    add kmonad x1

--- homeManagerConfigurations/jj/features/kmonad/default.nix
+++ homeManagerConfigurations/jj/features/kmonad/default.nix
@@ -3,39 +3,46 @@ let
 
   kmonad = inputs.kmonad.packages.${pkgs.system}.default;
 
+  kmonadConfigs ={
+    hhkb-usb = {
+      kdbFile = ./hhkb.kbd;
+      device = "/dev/input/by-id/usb-PFU_Limited_HHKB-Hybrid-event-kbd";
+    };
+    hhkb-blt = {
+      kdbFile = ./hhkb.kbd;
+      device = "/dev/input/event22";
+    };
+    x1 = {
+      kdbFile = ./x1.kbd;
+      device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+    };
+  };
 
-  kmonad-hhkb-usb = pkgs.writeShellScriptBin "kmonad-hhkb-usb"
-    (let
-      hhkb-usb = pkgs.substitute {
-        src = ./hhkb.kbd;
-        replacements = [ "--replace" "@device@" "/dev/input/by-id/usb-PFU_Limited_HHKB-Hybrid-event-kbd"
-                         "--replace" "@name@" "HHKB-USB"
-                       ];
+  kmonadScripts = lib.mapAttrs' (name: value: {
+    name = "kmonad-${name}";
+    value = pkgs.writeShellScriptBin "kmonad-${name}" (let
+      kbd = pkgs.substitute {
+        src = value.kdbFile;
+        replacements = [ "--replace" "@device@" "${value.device}" "--replace" "@name@" "${name}"];
       };
-    in "${kmonad}/bin/kmonad ${hhkb-usb}");
+    in "${kmonad}/bin/kmonad ${kbd}");
+  }) kmonadConfigs;
 
-  kmonad-hhkb-blt = pkgs.writeShellScriptBin "kmonad-hhkb-blt"
-    (let
-      hhkb-white = pkgs.substitute {
-        src = ./hhkb.kbd;
-        replacements = [ "--replace" "@device@" "/dev/input/event22"
-                         "--replace" "@name@" "HHKB-Bluetoot"
-                       ];
-      };
-    in "${kmonad}/bin/kmonad ${hhkb-white}");
+  myxset = pkgs.writeShellScriptBin "myxset" "xset r rate 300 80";
 
 in {
 
   home.packages = [
     kmonad
-    kmonad-hhkb-usb
-    kmonad-hhkb-blt
-  ];
+    myxset
+  ] ++ builtins.attrValues kmonadScripts;
 
   xsession.enable = true;
   xsession.initExtra = ''
     kmonad-hhkb-usb &
     kmonad-hhkb-blt &
+    kmonad-x1 &
+    myxset
     xset r rate 350 70
   '';
 
