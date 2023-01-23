8c3cc3f04d229c1671795cf2ebf2da3c4538104c
JJ Kim <jj@haedosa.xyz>
2023-01-23 18:37:39 +0900

    update xmonad (floatToRationalRect)

--- homeManagerConfigurations/jj/features/xmonad.nix
+++ homeManagerConfigurations/jj/features/xmonad.nix
@@ -1,9 +1,13 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
 
   imports = [ inputs.myxmonad.homeManagerModules.default ];
 
   mysystem.windowManager.xmonad.enable = true;
+
+  home.packages = [
+    inputs.myxmonad.packages.${pkgs.system}.xmonad-restart
+  ];
 
 }
